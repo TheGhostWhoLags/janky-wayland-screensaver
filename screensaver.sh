6e75bf120fe4bfe70db21a8c3c3f49488f0b5d6d
TheGhostWhoLags <118644556+TheGhostWhoLags@users.noreply.github.com>
2026-02-24 15:34:18 +1300

    Update screensaver.sh

--- screensaver.sh
+++ screensaver.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 #This script is designed to be called from an idle event trigger in hypridle or swayidle.  But it can be called manually or by any event or trigger you would like to use.
-#It spawns swayimg fullscreen to run a slideshow of images, then forks processes to monitor input from the libinput(keyboard & mouse), jstest(joypad), and the Dbus for any new inhibit locks whiie the screensaver is already active.
+#It spawns swayimg fullscreen to run a slideshow of images, then forks processes to monitor input from the libinput(keyboard & mouse), jstest(joypad), and the Dbus for new inhibit events while the screensaver is active.
 
 #This file is used to indicate there is an input from one of the three listeners.
 TEMPFILE=/tmp/inputdetect
 
-#TEMPFILE is used as a lock file.  If it already exists we will assume a copy of this script is currently running and exit.
+#TEMPFILE is also used as a lock file.  If it already exists we will assume a copy of this script is currently running and exit.
 if [ -f $TEMPFILE ]; then
   exit 1
 fi
 
-#Create the TEMPFILE to act as a lock and be ready for results from the input detection processes.
+#Create the TEMPFILE to act as a lockfile and ready for results from the input detection processes.
 touch $TEMPFILE
 
 #This causes any forked child processes to be killed when this script exits.  Since the screensaver app and input detectors are all forked, they all get cleaned up automatically. 
@@ -30,6 +30,7 @@ jstest --event /dev/input/js0 | grep -m1 "type 1,\|type 2," > $TEMPFILE &
 dbus-monitor | grep -m1 "interface=org.freedesktop.ScreenSaver; member=Inhibit" > $TEMPFILE &
 
 #inotifywait will block and wait until one of the listeners writes output to the temporary file.
-#systemd-inhibit stops the idle event that called this script from being triggered again while the screensaver is active.  It will probably block other idle triggers you have set from firing as well.  You don't need it if you depend on the conditional check for the existance of TEMPFILE at the start of this script.
-systemd-inhibit inotifywait -e modify $TEMPFILE
+#'systemd-inhibit --what=idle' stops the idle event that called this script from being triggered again while the screensaver is active.  It will probably block other idle triggers you have set from firing as well.  You don't need it if you depend on the conditional check for the existance of TEMPFILE at the start of this script.
+systemd-inhibit --what=idle inotifywait -e modify $TEMPFILE
+rm $TEMPFILE #TEMPFILE must be deleted or the conditional check at the start of this script will prevent it from running again.
 exit
