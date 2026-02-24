160fb98f5f92589fc54f1bb57b63f0ec92362777
TheGhostWhoLags <118644556+TheGhostWhoLags@users.noreply.github.com>
2026-02-24 14:58:52 +1300

    Update screensaver.sh
    Some improvements and simplifications.  The script checks if TEMPFILE already exists before running, and child forks are automatically killed when the script exits.

--- screensaver.sh
+++ screensaver.sh
@@ -2,31 +2,34 @@
 #This script is designed to be called from an idle event trigger in hypridle or swayidle.  But it can be called manually or by any event or trigger you would like to use.
 #It spawns swayimg fullscreen to run a slideshow of images, then forks processes to monitor input from the libinput(keyboard & mouse), jstest(joypad), and the Dbus for any new inhibit locks whiie the screensaver is already active.
 
-#This file is used to indicate there is an input from one of the three listeners.  It does not matter if it already exists or has existing content - it will be overwritten each time.
+#This file is used to indicate there is an input from one of the three listeners.
 TEMPFILE=/tmp/inputdetect
+
+#TEMPFILE is used as a lock file.  If it already exists we will assume a copy of this script is currently running and exit.
+if [ -f $TEMPFILE ]; then
+  exit 1
+fi
+
+#Create the TEMPFILE to act as a lock and be ready for results from the input detection processes.
 touch $TEMPFILE
 
-#Start and fork swayimg.  This is the 'screensaver'.  Running it in systemd-inhibit stops the idle event that called this script from being triggered again while the screensaver is active.
-#You could replace it with any process you want to use as a screensaver as long as it doesn't exit by itself without writing something to TEMPFILE.
-systemd-inhibit swayimg --config-file=/home/steam/.config/swayimg/config --fullscreen --scale=fill --slideshow --order=random /home/steam/.config/wallpaper &
+#This causes any forked child processes to be killed when this script exits.  Since the screensaver app and input detectors are all forked, they all get cleaned up automatically. 
+trap 'pkill --signal SIGTERM --parent $$' EXIT
 
-#Create and fork the listeners.  When one of them detects input it writes to the temporary file that inotifywait is watching.
-#You need to add the user calling this script to the 'input' group, or to whatever group owns the '/dev/input/*' devices on your system is if it is not 'input'.
+#Start and fork swayimg.  This is the 'screensaver'.
+#You could replace it with any process you want to use as a screensaver, as long as it doesn't exit by itself without writing something to TEMPFILE.
+swayimg --fullscreen --scale=fill --slideshow --order=random /home/steam/.config/wallpaper &
+
+#Create and fork the listeners.  When one of them detects input it writes to the TEMPFILE that inotifywait will be watching.
+#You need to add the user calling this script to the 'input' group, or to whatever group owns the '/dev/input/*' devices on your system is if that is not 'input'.
 libinput debug-events | grep -m1 "KEY\|MOTION" > $TEMPFILE &
 #This works for my controller.  If it doesn't for yours, run 'jstest --event /dev/input/js0' and press a few buttons to find out what event labels you need in the grep statement. 
 jstest --event /dev/input/js0 | grep -m1 "type 1,\|type 2," > $TEMPFILE &
-
-#This listens to D-Bus events that might be triggered behind the scenes while the screensaver is already running.  In my case, videos started on Vacuumtube via chromecast.
+#Listen for D-Bus events triggered behind the scenes while the screensaver is already running.  In my case, videos started on Vacuumtube via chromecast.
 #The 'org.freedesktop.ScreenSaver' string might be different in your Window Manager/Desktop Environment.  Run dbus-monitor, start playing a video, and find your needed string in the output.
 dbus-monitor | grep -m1 "interface=org.freedesktop.ScreenSaver; member=Inhibit" > $TEMPFILE &
 
 #inotifywait will block and wait until one of the listeners writes output to the temporary file.
-inotifywait -e modify $TEMPFILE
-sleep 0.2
-
-#Input has been detected, it's time to clean up any forked processes still running and then kill the swayimg 'screensaver'.
-pkill -f "jstest"
-pkill -f "libinput"
-pkill -f "dbus-monitor"
-pkill -f "swayimg" #As the systemd-inhibit/swayimg process has now closed, the idle timer can call this script again on the next idle timeout.
+#systemd-inhibit stops the idle event that called this script from being triggered again while the screensaver is active.  It will probably block other idle triggers you have set from firing as well.  You don't need it if you depend on the conditional check for the existance of TEMPFILE at the start of this script.
+systemd-inhibit inotifywait -e modify $TEMPFILE
 exit
